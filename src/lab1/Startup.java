4f0e21236edbcefb69d6a69602939284872b04fe
David <David@Arnell-PC.attlocal.net>
2015-02-02 00:07:27 -0600

    Advanced Java Lab for 2-2-2015 Final Recovered Version

--- src/lab1/Startup.java
+++ src/lab1/Startup.java
@@ -20,6 +20,25 @@ public class Startup {
             not have to be a GUI class.
         */
         Employee employee = new Employee();
+        //The employee information will stay in the "startup" class
+        //The information will be given to the HR Manager during the hiring
+        
+        HRManager hrManager = new HRManager();
+               
+        
+        //new instance of the HR Manager class is initiated
+        //I will have to review capitalization standards for acronyms in class
+        
+        //The employee information will be passed to the HRManager class
+        //I'm making the assumption that a request for this input is not needed
+        //The data here is constant
+        hrManager.hireNewEmployee("John", "Doe","333-33-3333");
+                
+        /*
+        The revised program will not be needing these lines of code
+        This information will be passed into the HRManager class
+        The employee registration has this information and it will
+        pass it to the necessary class
         employee.firstName = "Peter";
         employee.lastName = "Piper";
         employee.ssn = "333-1234";
@@ -27,6 +46,8 @@ public class Startup {
         employee.meetDepartmentStaff();
         employee.meetWithHrForBenefitAndSalryInfo();
         employee.reviewDeptPolicies();
+        */
+        
 
         System.out.println("The employee's status is: " + employee.getStatus());
     }
