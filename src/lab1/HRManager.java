b414c0f013c77e058d971bd970228bae644cbd99
David <David@Arnell-PC.attlocal.net>
2015-02-02 00:09:58 -0600

    Advanced Java Lab for 2-2-2015 Final Recovered Version

--- src/lab1/HRManager.java
+++ src/lab1/HRManager.java
@@ -5,10 +5,24 @@ package lab1;
  *
  * @author David
  */
-public class HRManager {
+
+//the HRManager class was created to handle new employee creation
+//The HR Manager "has a" the responsibility to hire someone
+public class HRManager {    
     
-    
-    
+    Employee employee = new Employee();
+        
+    public void hireNewEmployee(String firstName, String lastName, String snn){
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employee.setSsn(snn);
+        
+        //Call the methods in the Employee class
+        employee.meetWithHrForBenefitAndSalryInfo();
+        employee.meetDepartmentStaff();
+        employee.reviewDeptPolicies();
+        employee.moveIntoCubicle("Cube ID");        
+    }
     
     
 }
